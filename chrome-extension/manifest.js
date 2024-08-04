667e27fb569b3f4a5d6e6d7fde0c2823ec07958f
heytulsiprasad <tulsi.prasad50@gmail.com>
2024-08-04 19:37:38 +0530

    Fix basic setup of extension

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -3,15 +3,6 @@ import deepmerge from 'deepmerge';
 
 const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
-const isFirefox = process.env.__FIREFOX__ === 'true';
-
-const sidePanelConfig = {
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
-  permissions: ['sidePanel'],
-};
-
 /**
  * After changing, please reload the extension at `chrome://extensions`
  * @type {chrome.runtime.ManifestV3}
@@ -27,9 +18,8 @@ const manifest = deepmerge(
     name: '__MSG_extensionName__',
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
-    host_permissions: ['<all_urls>'],
-    permissions: ['storage', 'scripting'],
-    options_page: 'options/index.html',
+    host_permissions: ['*://x.com/*'],
+    permissions: ['storage'],
     background: {
       service_worker: 'background.iife.js',
       type: 'module',
@@ -38,27 +28,23 @@ const manifest = deepmerge(
       default_popup: 'popup/index.html',
       default_icon: 'icon-34.png',
     },
-    chrome_url_overrides: {
-      newtab: 'new-tab/index.html',
-    },
     icons: {
       128: 'icon-128.png',
     },
     content_scripts: [
       {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+        matches: ['*://x.com/*'],
         js: ['content/index.iife.js'],
       },
       {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+        matches: ['*://x.com/*'],
         js: ['content-ui/index.iife.js'],
       },
       {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+        matches: ['*://x.com/*'],
         css: ['content.css'], // public folder
       },
     ],
-    devtools_page: 'devtools/index.html',
     web_accessible_resources: [
       {
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
@@ -66,7 +52,7 @@ const manifest = deepmerge(
       },
     ],
   },
-  !isFirefox && sidePanelConfig,
+  {},
 );
 
 export default manifest;
